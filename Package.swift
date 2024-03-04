7d81750617ecc7ad308d1019d2af6ed93e586ff5
runner <runner@Mac-1709545392145.local>
2024-03-04 10:11:07 +0000

    KMM SPM package release for 0.0.1-ALPHA2.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/powersync-ja/powersync-kotlin/com/powersync/powersyncswift-kmmbridge/0.0.1-ALPHA2.1709546402639/powersyncswift-kmmbridge-0.0.1-ALPHA2.1709546402639.zip"
+let remoteKotlinChecksum = "713c4d3da38b8a946d22c8d594b1bcb8b9b0917832c46127664fd1af4612087d"
 let packageName = "powersyncswift"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./powersyncswift/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
