56c4724029a1fe174a71c296ab9f518eb05f6b75
DominicGBauer <dominic@nomanini.com>
2024-08-20 14:41:59 +0200

    chore: sonaetype plugin

--- PowerSync/build.gradle.kts
+++ PowerSync/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.kmmbridge)
     alias(libs.plugins.skie)
     alias(libs.plugins.mavenPublishPlugin)
-    signing
+    id("com.powersync.plugins.sonatype")
 }
 
 kotlin {
@@ -45,66 +45,6 @@ skie {
             SuspendInterop.Enabled(false)
         }
     }
-}
-
-group = "com.powersync"
-description = "PowerSync KMM Bridge"
-
-publishing {
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-
-            pom {
-                name.set(project.name)
-                description.set(project.description)
-                url.set("https://github.com/powersync-ja/powersync-kotlin")
-
-                developers {
-                    developer {
-                        id.set("journeyapps")
-                        name.set("Journey Mobile, Inc.")
-                        email.set("info@journeyapps.com")
-                    }
-                }
-
-                licenses {
-                    license {
-                        name.set("Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:github.com/powersync-ja/powersync-kotlin.git")
-                    developerConnection.set("scm:git:ssh://github.com/powersync-ja/powersync-kotlin.git")
-                    url.set("https://github.com/powersync-ja/powersync-kotlin")
-                }
-            }
-        }
-    }
-
-    val publishUsername= System.getenv("SONATYPE_USERNAME")
-    val publishPassword= System.getenv("SONATYPE_PASSWORD")
-
-    repositories {
-        maven {
-            name = "sonatype"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = publishUsername
-                password = publishPassword
-            }
-        }
-    }
-}
-
-signing {
-    val signingKey = System.getenv("SIGNING_KEY")
-    val signingPassword = System.getenv("SIGNING_PASSWORD")
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
 }
 
 kmmbridge {
