1bc0258aebe26e7941a228b198b80691826a8306
Manrich121 <manrich121@gmail.com>
2024-08-21 11:38:29 +0200

    wip: publish kmmbridget to sonatype portal

--- PowerSync/build.gradle.kts
+++ PowerSync/build.gradle.kts
@@ -1,3 +1,5 @@
+import co.touchlab.faktory.artifactmanager.ArtifactManager
+import co.touchlab.faktory.capitalized
 import co.touchlab.faktory.versionmanager.TimestampVersionManager
 import co.touchlab.skie.configuration.SuspendInterop
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
@@ -48,12 +50,83 @@ skie {
 }
 
 kmmbridge {
-    mavenPublishArtifacts()
+    artifactManager.set(SonatypePortalPublishArtifactManager(project, null, null, null))
+    artifactManager.finalizeValue()
     spm()
-    versionManager.set(TimestampVersionManager)
 }
 
 if (System.getenv().containsKey("CI")) {
     // Setup github publishing based on GitHub action variables
     addGithubPackagesRepository()
+}
+
+class SonatypePortalPublishArtifactManager(
+    val project: Project,
+    private val publicationName: String?,
+    artifactSuffix: String?,
+    private val repositoryName: String?,
+) : ArtifactManager {
+    private val FRAMEWORK_PUBLICATION_NAME = "KMMBridgeFramework"
+    private val KMMBRIDGE_ARTIFACT_SUFFIX = "kmmbridge"
+    private val group: String = project.group.toString().replace(".", "/")
+    private val kmmbridgeArtifactId =
+        "${project.name}-${artifactSuffix ?: KMMBRIDGE_ARTIFACT_SUFFIX}"
+
+    override fun deployArtifact(project: Project, zipFilePath: File, version: String): String {
+        return "FOOBAR_URL"
+    }
+
+    override fun configure(
+        project: Project,
+        version: String,
+        uploadTask: TaskProvider<Task>,
+        kmmPublishTask: TaskProvider<Task>
+    ) {
+        project.extensions.getByType<PublishingExtension>().publications.create(
+            publicationName ?: FRAMEWORK_PUBLICATION_NAME, MavenPublication::class.java
+        ) {
+            this.version = version
+            val archiveProvider = project.tasks.named("zipXCFramework", Zip::class.java).flatMap {
+                it.archiveFile
+            }
+            artifact(archiveProvider) {
+                extension = "zip"
+            }
+            artifactId = kmmbridgeArtifactId
+        }
+
+        publishingTasks().forEach {
+            uploadTask.configure {
+                dependsOn(it)
+            }
+        }
+        try {
+            project.tasks.named("publish").also { task ->
+                task.configure {
+                    dependsOn(kmmPublishTask)
+                }
+            }
+        } catch (_: UnknownTaskException) {
+            project.logger.warn("Gradle publish task not found")
+        }
+    }
+
+    private fun publishingTasks(): List<TaskProvider<Task>> {
+        val publishingExtension = project.extensions.getByType<PublishingExtension>()
+
+        // Either the user has supplied a correct name, or we use the default. If neither is found, fail.
+        val publicationNameCap =
+            publishingExtension.publications.getByName(
+                publicationName ?: FRAMEWORK_PUBLICATION_NAME
+            ).name.capitalized()
+
+        return publishingExtension.repositories.filterIsInstance<MavenArtifactRepository>()
+            .map { repo ->
+                val repositoryName = repo.name.capitalized()
+                val publishTaskName =
+                    "publish${publicationNameCap}PublicationTo${repositoryName}Repository"
+                // Verify that the "publish" task exists before collecting
+                project.tasks.named(publishTaskName)
+            }
+    }
 }
