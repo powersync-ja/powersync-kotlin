c53bd4ffc8402414ca67a58ee21021ce03871743
Dominic <dominic.g.bauer@gmail.com>
2024-10-10 16:43:36 +0200

    chore: update powersync sqlite core

--- demos/supabase-todolist/shared/build.gradle.kts
+++ demos/supabase-todolist/shared/build.gradle.kts
@@ -27,7 +27,7 @@ kotlin {
         ios.deploymentTarget = "14.1"
         podfile = project.file("../iosApp/Podfile")
         pod("powersync-sqlite-core") {
-            version = "0.2.1"
+            version = "0.3.0"
             linkOnly = true
         }
 
