548dec698526d0fd0c803d3b85d293b330478df8
Simon Binder <simon@journeyapps.com>
2025-02-21 15:29:46 +0100

    Revert unintentional changes

--- core/src/jvmTest/kotlin/com/powersync/testutils/TestUtils.jvm.kt
+++ core/src/jvmTest/kotlin/com/powersync/testutils/TestUtils.jvm.kt
@@ -4,7 +4,7 @@ import com.powersync.DatabaseDriverFactory
 import java.io.File
 
 actual val factory: DatabaseDriverFactory
-    get() =   DatabaseDriverFactory()
+    get() = DatabaseDriverFactory()
 
 actual fun cleanup(path: String) {
     File(path).delete()
