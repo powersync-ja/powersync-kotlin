8d2b8cc803bf6d816c37c2fe8fa015a020d91502
stevensJourney <steven@journeyapps.com>
2025-03-05 10:41:47 +0200

    delete shaky test test

--- core-tests-android/src/androidTest/java/com/powersync/AndroidDatabaseTest.kt
+++ core-tests-android/src/androidTest/java/com/powersync/AndroidDatabaseTest.kt
@@ -1,23 +1,18 @@
 package com.powersync
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import app.cash.turbine.turbineScope
 import com.powersync.db.schema.Schema
 import com.powersync.testutils.UserRow
+import kotlinx.coroutines.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.advanceTimeBy
 import org.junit.After
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.*
-
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AndroidDatabaseTest {
@@ -93,60 +88,6 @@ class AndroidDatabaseTest {
 
                 query.expectNoEvents()
                 query.cancel()
-            }
-        }
-
-        runTest {
-            turbineScope {
-                // Avoids skipping delays
-                withContext(Dispatchers.Default) {
-                    val query =
-                        database.watch(
-                            "SELECT * FROM users",
-                            throttleMs = 1000
-                        ) { UserRow.from(it) }.testIn(this)
-
-                    // Wait for initial query
-                    assertEquals(0, query.awaitItem().size)
-
-                    database.execute(
-                        "INSERT INTO users (id, name, email) VALUES (uuid(), ?, ?)",
-                        listOf("Test", "test@example.org"),
-                    )
-
-                    database.writeTransaction {
-                        it.execute(
-                            "INSERT INTO users (id, name, email) VALUES (uuid(), ?, ?)",
-                            listOf("Test2", "test2@example.org"),
-                        )
-                        it.execute(
-                            "INSERT INTO users (id, name, email) VALUES (uuid(), ?, ?)",
-                            listOf("Test3", "test3@example.org"),
-                        )
-                    }
-
-                    assertEquals(3, query.awaitItem().size)
-
-                    try {
-                        database.writeTransaction {
-                            it.execute("DELETE FROM users;")
-                            it.execute("syntax error, revert please")
-                        }
-                    } catch (e: Exception) {
-                        // Ignore
-                    }
-
-                    database.execute(
-                        "INSERT INTO users (id, name, email) VALUES (uuid(), ?, ?)",
-                        listOf("Test4", "test4@example.org"),
-                    )
-                    assertEquals(4, query.awaitItem().size)
-
-                    query.expectNoEvents()
-                    query.cancel()
-
-                }
-
             }
         }
 }
