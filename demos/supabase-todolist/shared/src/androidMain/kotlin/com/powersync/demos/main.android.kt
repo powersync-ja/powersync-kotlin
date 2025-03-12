6a2b99b6661a02c0d7c27b8c8036e1250ea25572
Simon Binder <simon@journeyapps.com>
2025-03-12 09:15:52 +0100

    Update Android dependencies

--- demos/supabase-todolist/shared/src/androidMain/kotlin/com/powersync/demos/main.android.kt
+++ demos/supabase-todolist/shared/src/androidMain/kotlin/com/powersync/demos/main.android.kt
@@ -1,9 +1,15 @@
 package com.powersync.demos
 
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.powersync.DatabaseDriverFactory
 
 @Composable
-fun MainView(factory: DatabaseDriverFactory) = App(factory, Modifier.fillMaxSize())
+fun MainView(factory: DatabaseDriverFactory) = App(
+    factory,
+    Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.systemBars)
+)
