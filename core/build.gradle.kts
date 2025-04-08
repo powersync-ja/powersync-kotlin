d8ac805592a73a1fe1494f0935681ead2574d057
Simon Binder <simon@journeyapps.com>
2025-04-08 10:36:55 +0200

    Fix framework path for iOS

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -171,7 +171,7 @@ kotlin {
                 linkerOpts("-framework", "powersync-sqlite-core")
                 val frameworkRoot =
                     binariesFolder
-                        .map { it.dir("framework/powersync-sqlite-core.xcframework/ios-arm64_x86_64-simulator") }
+                        .map { it.dir("framework/extracted/powersync-sqlite-core.xcframework/ios-arm64_x86_64-simulator") }
                         .get()
                         .asFile.path
 
