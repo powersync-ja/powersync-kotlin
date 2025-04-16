89ea92c3217eea12bc56a8a2b544bb13e6139eee
stevensJourney <steven@journeyapps.com>
2025-04-16 17:37:56 +0200

    update shared build plugin

--- plugins/build-plugin/src/main/kotlin/SharedBuildPlugin.kt
+++ plugins/build-plugin/src/main/kotlin/SharedBuildPlugin.kt
@@ -4,11 +4,12 @@ import de.undercouch.gradle.tasks.download.Download
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.konan.target.Family
+import java.io.File
 
 class SharedBuildPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -32,16 +33,24 @@ class SharedBuildPlugin : Plugin<Project> {
                 onlyIfModified(true)
             }
 
-        project.tasks.register("unzipPowersyncFramework", Copy::class.java) {
-            dependsOn(downloadPowersyncFramework)
+        val unzipPowersyncFramework =
+            project.tasks.register("unzipPowersyncFramework", Exec::class.java) {
+                dependsOn(downloadPowersyncFramework)
 
-            from(
-                project.zipTree(downloadPowersyncFramework.get().dest).matching {
-                    include("powersync-sqlite-core.xcframework/**")
-                },
-            )
-            into(binariesFolder.map { it.dir("framework") })
-        }
+                val zipfile = downloadPowersyncFramework.get().dest
+                inputs.file(zipfile)
+                val destination = File(zipfile.parentFile, "extracted")
+                doFirst {
+                    destination.deleteRecursively()
+                    destination.mkdir()
+                }
+
+                // We're using unzip here because the Gradle copy task doesn't support symlinks.
+                executable = "unzip"
+                args(zipfile.absolutePath)
+                workingDir(destination)
+                outputs.dir(destination)
+            }
 
         project.extensions
             .getByType(KotlinMultiplatformExtension::class.java)
@@ -56,12 +65,27 @@ class SharedBuildPlugin : Plugin<Project> {
                     binaries
                         .withType<org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable>()
                         .configureEach {
-                            linkTaskProvider.configure { dependsOn("unzipPowersyncFramework") }
+                            linkTaskProvider.configure { dependsOn(unzipPowersyncFramework) }
                             linkerOpts("-framework", "powersync-sqlite-core")
 
                             val frameworkRoot =
                                 binariesFolder
                                     .map { it.dir("framework/powersync-sqlite-core.xcframework/ios-arm64_x86_64-simulator") }
+                                    .get()
+                                    .asFile.path
+
+                            linkerOpts("-F", frameworkRoot)
+                            linkerOpts("-rpath", frameworkRoot)
+                        }
+                } else if (konanTarget.family == Family.OSX) {
+                    binaries
+                        .withType<org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable>()
+                        .configureEach {
+                            linkTaskProvider.configure { dependsOn("unzipPowersyncFramework") }
+                            linkerOpts("-framework", "powersync-sqlite-core")
+                            var frameworkRoot =
+                                binariesFolder
+                                    .map { it.dir("framework/extracted/powersync-sqlite-core.xcframework/macos-arm64_x86_64") }
                                     .get()
                                     .asFile.path
 
