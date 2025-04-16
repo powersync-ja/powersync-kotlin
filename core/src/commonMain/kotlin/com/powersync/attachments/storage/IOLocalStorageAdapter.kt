f729d426c63a9bce1215b497f56ecc557a22f916
stevensJourney <steven@journeyapps.com>
2025-04-16 16:58:49 +0200

    use fileSystem SystemFileSystem on IOLocalStorageAdapter

--- core/src/commonMain/kotlin/com/powersync/attachments/storage/IOLocalStorageAdapter.kt
+++ core/src/commonMain/kotlin/com/powersync/attachments/storage/IOLocalStorageAdapter.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import kotlinx.io.Buffer
 import kotlinx.io.buffered
+import kotlinx.io.files.FileSystem
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 
@@ -19,6 +20,8 @@ import kotlinx.io.files.SystemFileSystem
  * Storage adapter for local storage using the KotlinX IO library
  */
 public class IOLocalStorageAdapter : LocalStorage {
+    private val fileSystem: FileSystem = SystemFileSystem
+
     public override suspend fun saveFile(
         filePath: String,
         data: Flow<ByteArray>,
@@ -26,7 +29,7 @@ public class IOLocalStorageAdapter : LocalStorage {
         runWrappedSuspending {
             withContext(Dispatchers.IO) {
                 var totalSize = 0L
-                SystemFileSystem.sink(Path(filePath)).use { sink ->
+                fileSystem.sink(Path(filePath)).use { sink ->
                     // Copy to a buffer in order to write
                     Buffer().use { buffer ->
                         data.collect { chunk ->
@@ -48,7 +51,7 @@ public class IOLocalStorageAdapter : LocalStorage {
         mediaType: String?,
     ): Flow<ByteArray> =
         flow {
-            SystemFileSystem.source(Path(filePath)).use { source ->
+            fileSystem.source(Path(filePath)).use { source ->
                 source.buffered().use { bufferedSource ->
                     var remaining = 0L
                     val bufferSize = 8192L
@@ -64,35 +67,35 @@ public class IOLocalStorageAdapter : LocalStorage {
     public override suspend fun deleteFile(filePath: String): Unit =
         runWrappedSuspending {
             withContext(Dispatchers.IO) {
-                SystemFileSystem.delete(Path(filePath))
+                fileSystem.delete(Path(filePath))
             }
         }
 
     public override suspend fun fileExists(filePath: String): Boolean =
         runWrappedSuspending {
             withContext(Dispatchers.IO) {
-                SystemFileSystem.exists(Path(filePath))
+                fileSystem.exists(Path(filePath))
             }
         }
 
     public override suspend fun makeDir(path: String): Unit =
         runWrappedSuspending {
             withContext(Dispatchers.IO) {
-                SystemFileSystem.createDirectories(Path(path))
+                fileSystem.createDirectories(Path(path))
             }
         }
 
     public override suspend fun rmDir(path: String): Unit =
         runWrappedSuspending {
             withContext(Dispatchers.IO) {
-                for (item in SystemFileSystem.list(Path(path))) {
+                for (item in fileSystem.list(Path(path))) {
                     // Can't delete directories with files in them. Need to go down the file tree
                     // and clear the directory.
-                    val meta = SystemFileSystem.metadataOrNull(item)
+                    val meta = fileSystem.metadataOrNull(item)
                     if (meta?.isDirectory == true) {
                         rmDir(item.toString())
                     } else if (meta?.isRegularFile == true) {
-                        SystemFileSystem.delete(item)
+                        fileSystem.delete(item)
                     }
                 }
             }
@@ -104,8 +107,8 @@ public class IOLocalStorageAdapter : LocalStorage {
     ): Unit =
         runWrappedSuspending {
             withContext(Dispatchers.IO) {
-                SystemFileSystem.source(Path(sourcePath)).use { source ->
-                    SystemFileSystem.sink(Path(targetPath)).use { sink ->
+                fileSystem.source(Path(sourcePath)).use { source ->
+                    fileSystem.sink(Path(targetPath)).use { sink ->
                         source.buffered().transferTo(sink.buffered())
                     }
                 }
