a1620cdfb8f8bb34642ef54dd0f0b5fea580b0bd
stevensJourney <51082125+stevensJourney@users.noreply.github.com>
2025-04-30 13:53:51 +0200

    [Internal] Swift Exception Helper (#184)

--- PowerSyncKotlin/src/appleMain/kotlin/com/powersync/SDK.kt
+++ PowerSyncKotlin/src/appleMain/kotlin/com/powersync/SDK.kt
@@ -1,5 +1,15 @@
-@file:Suppress("ktlint:standard:no-empty-file")
-
 // This is required to build the iOS framework
 
 package com.powersync
+
+/**
+ * Helper class designed to bridge SKIEE methods and allow them to throw
+ * `PowerSyncException`. This is necessary because these exceptions cannot
+ * be thrown directly in Swift.
+ *
+ * The class provides a mechanism to handle exceptions in a way that is
+ * compatible with the Swift environment, ensuring proper error propagation
+ * and handling.
+ */
+@Throws(PowerSyncException::class)
+public fun throwPowerSyncException(exception: PowerSyncException): Unit = throw exception
