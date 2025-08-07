68f8f4cb0e7069164253600b47c2ea3260a43027
Oleg Koretsky <oleg@radiokot.com.ua>
2025-08-07 23:05:44 +0300

    Do not modify global Kermit severity

--- core/src/commonMain/kotlin/com/powersync/utils/Log.kt
+++ core/src/commonMain/kotlin/com/powersync/utils/Log.kt
@@ -1,24 +1,21 @@
 package com.powersync.utils
 
-import co.touchlab.kermit.Logger
-import co.touchlab.kermit.Severity
+import BuildConfig
+import co.touchlab.kermit.*
 
 /*
  * Generates a logger with the appropriate severity level based on the build type
  * if no Logger is provided.
 */
 public fun generateLogger(logger: Logger?): Logger {
-    if (logger != null) {
-        return logger
-    }
-
-    val defaultLogger: Logger = Logger
-
-    if (BuildConfig.isDebug) {
-        Logger.setMinSeverity(Severity.Verbose)
-    } else {
-        Logger.setMinSeverity(Severity.Warn)
-    }
-
-    return defaultLogger
+    return logger
+        ?: Logger(
+            config = StaticConfig(
+                logWriterList = listOf(platformLogWriter()),
+                minSeverity = if (BuildConfig.isDebug)
+                    Severity.Verbose
+                else
+                    Severity.Warn
+            ),
+        )
 }
