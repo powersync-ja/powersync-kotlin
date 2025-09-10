75cc731e0ba0de4d0001ef391bda20ba4118edda
Simon Binder <simon@journeyapps.com>
2025-09-10 11:53:23 +0200

    Remove android unit tests

--- integrations/room/src/androidUnitTest/kotlin/com/powersync/integrations/room/Utils.android.kt
+++
@@ -1,7 +0,0 @@
-package com.powersync.integrations.room
-
-import androidx.room.RoomDatabase
-
-actual fun createDatabaseBuilder(): RoomDatabase.Builder<TestDatabase> {
-    TODO("Android unit tests are unsupported, we test on JVM instead")
-}
