eab8a9daff6b00746e7269c9c0b8edcd4df0bc3e
Simon Binder <simon@journeyapps.com>
2025-09-30 13:18:50 +0200

    Move to integration test

--- integrations/supabase/build.gradle.kts
+++ integrations/supabase/build.gradle.kts
@@ -32,11 +32,29 @@ kotlin {
             api(libs.supabase.storage)
         }
 
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
-            implementation(libs.test.coroutines)
-            implementation(libs.test.kotest.assertions)
+        val commonIntegrationTest by creating {
+            dependsOn(commonTest.get())
+
+            dependencies {
+                // Separate project because SQLDelight can't generate code in test source sets.
+                implementation(projects.integrations.sqldelightTestDatabase)
+
+                implementation(libs.kotlin.test)
+                implementation(libs.kotlinx.io)
+                implementation(libs.test.turbine)
+                implementation(libs.test.coroutines)
+                implementation(libs.test.kotest.assertions)
+
+                implementation(libs.sqldelight.coroutines)
+            }
         }
+
+        // The PowerSync SDK links the core extension, so we can just run tests as-is.
+        jvmTest.get().dependsOn(commonIntegrationTest)
+
+        // We have special setup in this build configuration to make these tests link the PowerSync extension, so they
+        // can run integration tests along with the executable for unit testing.
+        nativeTest.orNull?.dependsOn(commonIntegrationTest)
     }
 }
 
