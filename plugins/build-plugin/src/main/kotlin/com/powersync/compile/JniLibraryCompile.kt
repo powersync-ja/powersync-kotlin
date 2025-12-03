3c892d480e38a8de4e0e561312c5606e6790db23
Simon Binder <simon@journeyapps.com>
2025-12-03 09:32:39 +0100

    Improve error message on clang failure

--- plugins/build-plugin/src/main/kotlin/com/powersync/compile/JniLibraryCompile.kt
+++ plugins/build-plugin/src/main/kotlin/com/powersync/compile/JniLibraryCompile.kt
@@ -16,6 +16,8 @@ import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ProcessExecutionException
+import org.gradle.process.internal.ExecException
 import java.io.File
 import javax.inject.Inject
 
@@ -79,59 +81,74 @@ abstract class JniLibraryCompile: DefaultTask() {
     fun run() {
         val target = this.target.get()
         val clang = clangPath.getOrElse("clang")
+        val workingDir = layout.projectDirectory.asFile
 
-        providers.exec {
-            workingDir = layout.projectDirectory.asFile
+        fun filePath(file: File): String {
+            return file.relativeTo(workingDir).path
+        }
 
-            fun filePath(file: File): String {
-                return file.relativeTo(workingDir).path
-            }
-
+        val args = buildList {
             if (target == JniTarget.LINUX_X64) {
-                args("-fuse-ld=lld")
-                args("--sysroot=build/sysroot/")
+                add("-fuse-ld=lld")
+                add("--sysroot=build/sysroot/")
             }
 
             if (target == JniTarget.LINUX_ARM) {
                 val gccParent = layout.buildDirectory.dir("sysroot/usr/lib/gcc/aarch64-linux-gnu")
                 val gccPath = filePath(gccParent.get().asFile.listFiles().single())
 
-                args("-fuse-ld=lld")
-                args("--sysroot=build/sysroot/usr/aarch64-linux-gnu/")
-                args("-Wl,-L", gccPath)
-                args("-B", gccPath)
+                add("-fuse-ld=lld")
+                add("--sysroot=build/sysroot/usr/aarch64-linux-gnu/")
+                add("-Wl,-L")
+                add(gccPath)
+                add("-B")
+                add(gccPath)
             }
 
-            executable = clang
-            toolchain.orNull?.let { args("-B$toolchain") }
+            toolchain.orNull?.let { add("-B$toolchain") }
 
-            args(
-                "-shared",
-                "-fPIC",
-                when (target) {
-                    JniTarget.LINUX_ARM -> "--target=aarch64-pc-linux"
-                    JniTarget.LINUX_X64 -> "--target=x86_64-pc-linux"
-                    JniTarget.MACOS_ARM -> "--target=aarch64-apple-macos"
-                    JniTarget.MACOS_X64 -> "--target=x86_64-apple-macos"
-                    JniTarget.WINDOWS_ARM -> "--target=aarch64-w64-mingw32uwp"
-                    JniTarget.WINDOWS_X64 -> "--target=x86_64-w64-mingw32uwp"
-                },
-                "-o",
-                filePath(sharedLibrary.get().asFile),
-                *inputFiles.files.map { filePath(it) }.toTypedArray(),
-                "-I",
-                filePath(include.get().asFile),
-                "-I",
-                "jni/headers/common",
-                "-I",
-                when (target) {
-                    JniTarget.LINUX_X64, JniTarget.LINUX_ARM -> "jni/headers/inc_linux"
-                    JniTarget.MACOS_X64, JniTarget.MACOS_ARM -> "jni/headers/inc_mac"
-                    JniTarget.WINDOWS_X64, JniTarget.WINDOWS_ARM -> "jni/headers/inc_win"
-                },
-                "-O3",
-                *ClangCompile.sqlite3ClangOptions,
-            )
-        }.result.get()
+            add("-shared")
+            add("-fPIC")
+            add(when (target) {
+                JniTarget.LINUX_ARM -> "--target=aarch64-pc-linux"
+                JniTarget.LINUX_X64 -> "--target=x86_64-pc-linux"
+                JniTarget.MACOS_ARM -> "--target=aarch64-apple-macos"
+                JniTarget.MACOS_X64 -> "--target=x86_64-apple-macos"
+                JniTarget.WINDOWS_ARM -> "--target=aarch64-w64-mingw32uwp"
+                JniTarget.WINDOWS_X64 -> "--target=x86_64-w64-mingw32uwp"
+            })
+            add("-o")
+            add(filePath(sharedLibrary.get().asFile))
+            inputFiles.files.forEach { add(filePath(it)) }
+            add("-I")
+            add(filePath(include.get().asFile))
+            add("-I")
+            add("jni/headers/common")
+            add("-I")
+            add(when (target) {
+                JniTarget.LINUX_X64, JniTarget.LINUX_ARM -> "jni/headers/inc_linux"
+                JniTarget.MACOS_X64, JniTarget.MACOS_ARM -> "jni/headers/inc_mac"
+                JniTarget.WINDOWS_X64, JniTarget.WINDOWS_ARM -> "jni/headers/inc_win"
+            })
+            add("-O3")
+            addAll(ClangCompile.sqlite3ClangOptions)
+        }
+
+        val execProvider = providers.exec {
+            this.workingDir = workingDir
+            executable = clang
+            args(args)
+
+            isIgnoreExitValue = true // So that we can provide better error messages
+        }
+
+        try {
+            execProvider.result.get().assertNormalExitValue()
+        } catch (_: ProcessExecutionException) {
+            val formattedArgs = args.joinToString(separator = " ") { "\"$it\"" }
+            val stderr = execProvider.standardError.asText.orNull
+
+            throw ProcessExecutionException("Could not start $clang $formattedArgs. Stderr: $stderr")
+        }
     }
 }
