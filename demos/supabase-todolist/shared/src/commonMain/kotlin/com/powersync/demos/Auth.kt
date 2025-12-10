33d5b016d3a4c1395595a3d182b12b250ff4b9ce
stevensJourney <51082125+stevensJourney@users.noreply.github.com>
2025-12-10 10:24:14 +0200

    feat: add appMetadata for service logs (#292)

--- demos/supabase-todolist/shared/src/commonMain/kotlin/com/powersync/demos/Auth.kt
+++ demos/supabase-todolist/shared/src/commonMain/kotlin/com/powersync/demos/Auth.kt
@@ -69,6 +69,9 @@ internal class AuthViewModel(
                                                 }
                                             },
                                     ),
+                                appMetadata = mapOf(
+                                    "appVersion" to Config.APP_VERSION
+                                ),
                             )
                         }
 
