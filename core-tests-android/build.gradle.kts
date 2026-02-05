8eac9f7097860a9d3cf23aad451ceecc2c5a04d9
Simon Binder <simon@journeyapps.com>
2026-02-05 09:09:52 +0100

    Support latest Kotlin and Ktor versions (#317)

--- core-tests-android/build.gradle.kts
+++ core-tests-android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -45,7 +47,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
     }
 }
